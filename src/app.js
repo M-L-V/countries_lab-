f9992aa10d3c2f1e35f0962959ac6fe312c381ef
Morgaine <morgainedv@gmail.com>
2019-11-26 15:33:50 +0000

    adding find method

--- src/app.js
+++ src/app.js
@@ -4,25 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      countries: []
+      countries: [],
+      filterSearch: "",
     },
     mounted(){
       this.fetchCountry();
     },
+
     computed: {
         displayTotalPopulation: function(){
           return this.countries.reduce((total, country) => total + country.population, 0);
-        }
+        },
+
+        findCountry: function(){
+          return this.countries.filter((country) => {
+          return (country.name === this.filterSearch ? country : "");
+        })
+      }
     },
 
-    methods: {
+      methods: {
       fetchCountry: function(){
         const request = fetch("https://restcountries.eu/rest/v2/all")
         .then(response => response.json())
         .then(data => this.countries = (data))
-      },
+      }
 
     }
+
   })
 
-})
+});
